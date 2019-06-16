d654ecfac83eb886e456612c25443081f5eca96e
aboulmagdApp <aboulmagd@live.com>
2019-06-16 17:26:55 +0300

    Using sql server with entity framework core

--- Startup.cs
+++ Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using aspnetcoreNewWeb.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace aspnetcoreNewWeb
 {
@@ -18,6 +19,7 @@ namespace aspnetcoreNewWeb
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContextPool<AppDbContext>(options => options.UseSqlServer(_config.GetConnectionString("EmployeeDBConnection")));
             services.AddMvc();
             services.AddSingleton<IEmployeeRepository, MockEmployeeRepository>();
         }
